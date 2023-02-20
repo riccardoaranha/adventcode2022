1937f9df4588ebb2e4a4d1231683926a4ed0593e
riccardoaranha <riccardoaranha@gmail.com>
2023-02-21 00:05:32 +0100

    Day 09 implementation  Changes to be committed: 	new file:   day09/day09.py 	new file:   day09/example.txt 	new file:   day09/input.txt 	new file:   day09/task/Day 9 - Advent of Code 2022.html 	new file:   day09/task/Day 9 - Advent of Code 2022_files/css 	new file:   day09/task/Day 9 - Advent of Code 2022_files/highcontrast.css 	new file:   day09/task/Day 9 - Advent of Code 2022_files/style.css 	modified:   test_results.py

---
+++ day09/day09.py
@@ -0,0 +1,74 @@
+import os, sys, io, argparse
+
+class Point(object):
+    COUNT = 0
+    def __init__(self, x, y):
+        self.X = x
+        self.Y = y
+
+    def move(self, direction):
+        if direction == 'U':
+            self.Y += 1 
+        elif direction == 'D':
+            self.Y += -1
+        elif direction == 'L':
+            self.X += -1
+        elif direction == 'R':
+            self.X += 1
+        else:
+            raise Exception("Invalid input")
+
+    def moveTowards(self, p):
+        if abs(self.X - p.X) == 2 or abs(self.Y - p.Y) == 2:
+            self.X += int((p.X - self.X) / (1 if (p.X == self.X) else abs(p.X - self.X)))
+            self.Y += int((p.Y - self.Y) / (1 if (p.Y == self.Y) else abs(p.Y - self.Y)))
+            
+
+def main(f: io.TextIOWrapper):
+    knots_count = 10
+    lines = f.readlines()
+    knots = [Point(0, 0) for i in range(0, knots_count)]  #array of 9 tails
+    unique = set()
+    unique.add((0, 0))
+    unique2 = set()
+    unique2.add((0, 0))
+    for line in lines:
+        line = line[0:-1]
+        x = line.split(' ')
+        #print(line)
+        for i in range(0, int(x[1])):
+            knots[0].move(x[0]) #moves the head
+            #print('Head', 0, knots[0].X, knots[0].Y)
+
+            for j in range(1, knots_count):
+                knots[j].moveTowards(knots[j-1])
+                #print('Tail', j, knots[j].X, knots[j].Y)
+            unique.add((knots[1].X, knots[1].Y))
+            unique2.add((knots[9].X, knots[9].Y))
+            
+
+    part1 = len(unique)
+    part2 = len(unique2)
+    print(part1, part2)
+    return {'part1': part1, 'part2' : part2}
+
+
+def is_valid_file(parser, arg):
+    if not os.path.exists(arg):
+        parser.error("The file %s does not exist!" % arg)
+    else:
+        return open(arg, 'r')  # return an open file handle
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog = 'dayXX',
+                                     description = 'Advent of Code Day XX Template Start ',
+                                     epilog = 'Created by Riccardo Aranha')
+    parser.add_argument('inputfile', 
+                        nargs='?', 
+                        default=os.path.join(sys.path[0], "input.txt"), 
+                        metavar="FILE",
+                        type=lambda x: is_valid_file(parser, x))
+
+    args = parser.parse_args()
+    main(args.inputfile)
+    args.inputfile.close()
--- test_results.py
+++ test_results.py
@@ -1,4 +1,4 @@
-from unittest import TestCase
+import unittest  
 
 from day01.day01 import main as day01
 from day02.day02 import main as day02
@@ -8,34 +8,43 @@ from day05.day05 import main as day05
 from day06.day06 import main as day06
 from day07.day07 import main as day07
 from day08.day08 import main as day08
+from day09.day09 import main as day09
 
-def dayXX(day, e1, e2, i1, i2):
-    f = open('day%02d/example.txt' % day, 'r')
-    TestCase().assertDictEqual( {'part1': e1, 'part2' : e2}, globals()['day%02d' % day](f))
-    
-    f = open('day%02d/input.txt' % day, 'r')
-    TestCase().assertDictEqual( {'part1': i1, 'part2' : i2}, globals()['day%02d' % day](f))
 
-def test_day01():
-    dayXX(1, 24000, 45000, 75622, 213159)
+class TestResults(unittest.TestCase):
+    def dayXX(self, day, e1, e2, i1, i2):
+        f = open('day%02d/example.txt' % day, 'r')
+        unittest.TestCase().assertDictEqual( {'part1': e1, 'part2' : e2}, globals()['day%02d' % day](f))
+        
+        f = open('day%02d/input.txt' % day, 'r')
+        unittest.TestCase().assertDictEqual( {'part1': i1, 'part2' : i2}, globals()['day%02d' % day](f))
 
-def test_day02():
-    dayXX(2, 15, 12, 13052, 13693)
+    def test_day01(self):
+        self.dayXX(1, 24000, 45000, 75622, 213159)
 
-def test_day03():
-    dayXX(3, 157, 70, 7737, 2697)
+    def test_day02(self):
+        self.dayXX(2, 15, 12, 13052, 13693)
 
-def test_day04():
-    dayXX(4, 2, 4, 475, 825)
+    def test_day03(self):
+        self.dayXX(3, 157, 70, 7737, 2697)
 
-def test_day05():
-    dayXX(5, 'CMZ', 'MCD', 'TLNGFGMFN', 'FGLQJCMBD')
+    def test_day04(self):
+        self.dayXX(4, 2, 4, 475, 825)
 
-def test_day06():
-    dayXX(6, 11, 26, 1757, 2950)
+    def test_day05(self):
+        self.dayXX(5, 'CMZ', 'MCD', 'TLNGFGMFN', 'FGLQJCMBD')
 
-def test_day07():
-    dayXX(7, 95437, 24933642, 2031851, 2568781)
+    def test_day06(self):
+        self.dayXX(6, 11, 26, 1757, 2950)
 
-def test_day08():
-    dayXX(8, 21, 8, 1543, 595080)
+    def test_day07(self):
+        self.dayXX(7, 95437, 24933642, 2031851, 2568781)
+
+    def test_day08(self):
+        self.dayXX(8, 21, 8, 1543, 595080)
+
+    def test_day09(self):
+        self.dayXX(9, 13, 1, 6464, 2604)
+
+if __name__ == '__main__':
+    unittest.main()
